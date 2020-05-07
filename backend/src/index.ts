2b5ecfc9dafd3d6851d7694f440e95b7a31b351c
shinystar <creditcoder@hotmail.com>
2020-05-06 22:31:36 -0400

    replace backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,51 +1,27 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import { Edition } from "./entity/Edition";
-import { Section } from "./entity/Section";
-import { serialize } from "class-transformer";
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import * as helmet from "helmet";
+import * as cors from "cors";
+import routes from "./routes";
 
-createConnection({
-  type: "mysql",
-  host: "127.0.0.1",
-  port: 3306,
-  username: "root",
-  password: "1234",
-  database: "test_db",
-  entities: [__dirname + "/entity/*.ts"],
-  synchronize: true,
-  logging: false,
-})
+//Connects to the Database -> then starts the express
+createConnection()
   .then(async (connection) => {
-    let edition1 = new Edition();
-    edition1.name = "January February 2019";
-    await connection.manager.save(edition1);
+    // Create a new express application instance
+    const app = express();
 
-    let edition2 = new Edition();
-    edition2.name = "November December 2018";
-    await connection.manager.save(edition2);
+    // Call midlewares
+    app.use(cors());
+    app.use(helmet());
+    app.use(bodyParser.json());
 
-    let section = new Section();
-    section.name = "Application Development";
+    //Set all routes from routes folder
+    app.use("/", routes);
 
-    section.editions = [edition1, edition2];
-    await connection.manager.save(section);
-
-    let section2 = new Section();
-    section2.name = "DBA";
-
-    section2.editions = [edition1];
-    await connection.manager.save(section2);
-
-    let section3 = new Section();
-    section3.name = "Database Developer";
-
-    section3.editions = [edition1];
-    await connection.manager.save(section3);
-
-    const loadedSection = await connection
-      .getRepository(Section)
-      .findOne(1, { relations: ["editions"] });
-
-    console.log(serialize(loadedSection));
+    app.listen(8000, () => {
+      console.log("Server started on port 8000!");
+    });
   })
   .catch((error) => console.log(error));
